a81de65fd477005a9b98ea381f82ca605afec941
aa775319114 <satihayashi1@gmail.com>
2024-12-22 17:08:48 +0200

    666

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let productsGrid = document.getElementById('products-grid');
 let productsArray = [];
-let cartProd = document.getElementById('cart-products')
-let cart = []
 let xhr = new XMLHttpRequest();
 let url = 'https://my-json-server.typicode.com/SatoHayashi1/Vitalik';
-let my_headers = {
-    "context-type": "application/json",
-    "x-apikey": "6768232ee705334d8c3584ec",
-    "cache-control": "no-cache"
-}
 xhr.open('GET',url + '/products');
-xhr.responseType = 'json',
+xhr.setRequestHeader("content-type", "application/json");
+xhr.setRequestHeader("x-apikey", "6768232ee705334d8c3584ec");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.responseType = 'json'
 xhr.onload = function() {
     productsArray = xhr.response
     productsGrid.innerHTML = null;
@@ -23,58 +19,52 @@ xhr.onload = function() {
             <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
             <p class='product-price'><b>Price: </b>${p.price}$</p>
             <p class='product-description'><b>Description: </b>${p.description}</p>
-            <a href='userProfile.html?id=${p.author_id}'>Seller profile</a>
-            <button onclick="addProductToCart(${p.id})">Buy</button>
+            <button onclick="addProductToCart('${p._id}')">Buy</button>
         `;
         productsGrid.append(pElem);
     });
 }
 xhr.send();
-
+// CART ----------------
+let cartProd = document.getElementById('cart-products');
+let cart = [];
+if(localStorage.getItem('cart')) {
+    cart = JSON.parse(localStorage.getItem('cart'));
+    drawCartProducts();
+}
+function addProductToCart(id) {
+    let product = productsArray.find(function(p) {
+        return p._id == id;
+    })
+    cart.push(product);
+    drawCartProducts();
+    localStorage.setItem("cart", JSON.stringify(cart));
+    document.getElementById('cart-button').classList.add('active');
+    setTimeout(function(){
+        document.getElementById('cart-button').classList.remove('active');
+    },500);
+}
+function drawCartProducts() {
+    if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
+    cartProd.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
+            <hr>
+        `;
+        sum += p.price;
+    });
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+function buyAll() {
+    cart = [];
+    cartProd.innerHTML = 'Money was withdrawn from your credit card';
+    localStorage.setItem("cart", '[]');
+}
 function openCart() {
     cartProd.classList.toggle('hide');
 }
-
-function addProductToCart(id) {
-    let product = productsArray.find(function(p) {
-        return p.id == id;
-    })
-    cart.push(product)
-}
-if(localStorage.getItem('cart')) {
-         cart = JSON.parse(localStorage.getItem('cart'));
-         drawCartProducts();
-    }
-    function addProductToCart(id) {
-        let product = productsArray.find(function(p) {
-            return p.id == id;
-        })
-        cart.push(product);
-        drawCartProducts();
-        localStorage.setItem("cart", JSON.stringify(cart));
-        document.getElementById('cart-button').classList.add('active');
-        setTimeout(function(){
-            document.getElementById('cart-button').classList.remove('active');
-        },500);
-    }
-    function drawCartProducts() {
-        if(cart.length === 0) return cartProd.innerHTML = 'Cart is empty';
-        cartProd.innerHTML = null;
-        let sum = 0;
-        cart.forEach(function(p){
-            cartProd.innerHTML += `
-                <p><img src="${p.photo_url}"> ${p.name} |${p.price}$</p>
-                <hr>
-            `;
-            sum += p.price;
-        });
-        cartProd.innerHTML += `
-            <p>Total Price: ${sum}$</p>
-            <button onclick="buyAll()">Buy All</button>
-        `;
-    }
-    function buyAll() {
-        cart = [];
-        cartProd.innerHTML = 'Money was withdrawn from your credit card';
-        localStorage.setItem("cart", '[]');
-    }
